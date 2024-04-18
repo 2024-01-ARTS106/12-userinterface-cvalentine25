78b0f229b16660a933fd306c62829d810d869507
cvalentine25 <156930887+cvalentine25@users.noreply.github.com>
2024-04-17 22:48:34 -0400

    HOME17April

--- Assets/RW/Scripts/UIManager.cs
+++ Assets/RW/Scripts/UIManager.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class UIManager : MonoBehaviour
 
 {
-
+public Animator StartButton;
+public Animator SettingsButton; 
     // Start is called before the first frame update
 
 
-
+    public void OpenSettings()
+    {
+        StartButton.SetBool("isHidden",true);
+        SettingsButton.SetBool("isHidden", true);
+    }
     public void StartGame()
     {
 
